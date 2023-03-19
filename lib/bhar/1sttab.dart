2bf6851adc478ccd5f32f7f146f0746f31774902
buhar <bhar.mayi01@gmail.com>
2023-03-19 17:05:42 +0300

    aalist crt

--- lib/bhar/1sttab.dart
+++ lib/bhar/1sttab.dart
@@ -10,11 +10,24 @@ Aaa({
  required this.timeinf,
  required this.num
 
-
  });
 
-
-
-
-
 }
+
+
+
+List<Aaa> aalist=[
+ Aaa(sections: "sss", lecname: "Course overview", timeinf: "Video- 04:34 mins", num: 1),
+ Aaa(sections: "sss", lecname: "Installing XAMPP on Mac", timeinf: "Video- 03:40 mins", num: 2),
+ Aaa(sections: "sss", lecname: "Installing a free code editor (Optional)", timeinf: "Video- 05:04 mins", num: 3),
+ Aaa(sections: "sss", lecname: "PHP code in HTML", timeinf: "Video- 02:00 mins", num: 4),
+ Aaa(sections: "sss", lecname: "Phpinfo", timeinf: "Video- 03:33 mins", num: 5),
+ Aaa(sections: "sss", lecname: "Using Echo statement in PHP", timeinf: "Video- 02:13 mins", num: 6),
+ Aaa(sections: "sss", lecname: "Using Print statement in PHP", timeinf: "Video- 01:22 mins", num: 7),
+ Aaa(sections: "sss", lecname: "Adding comments in PHP", timeinf: "Video- 01:01 mins", num: 8),
+ Aaa(sections: "sss", lecname: "Line break", timeinf: "Video- 02:00 mins", num: 9),
+ Aaa(sections: "sss", lecname: "Variables in PHP", timeinf: "Video- 06:13 mins", num: 10),
+ Aaa(sections: "sss", lecname: "Multiple ways to add variables", timeinf: "Video- 02:02 mins", num: 11),
+ Aaa(sections: "sss", lecname: "Data types", timeinf: "Video- 05:34 mins", num: 12),
+
+ ];
