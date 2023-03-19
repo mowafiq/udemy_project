d43812eefcc3fe6add7984c43c63c6f6bf245ee0
buhar <bhar.mayi01@gmail.com>
2023-03-19 17:12:05 +0300

    2nd list cls

---
+++ lib/bhar/2ndtab.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Bbb{
+String nn;
+Icon a;
+Bbb({
+required this.nn,
+required this.a,
+
+
+
+});
+
+}
+
+List<Bbb> bblist=[
+Bbb(nn: "About this Course", a: Icon(Icons.more_horiz)),
+Bbb(nn: "Share this Course", a: Icon(Icons.file_upload_outlined)),
+Bbb(nn: "Notes", a: Icon(Icons.event_note)),
+Bbb(nn: "Resources", a: Icon(Icons.format_list_bulleted)),
+Bbb(nn: "Announcements", a: Icon(Icons.notifications_none)),
+Bbb(nn: "Add course to favorites", a: Icon(Icons.star_outline_rounded)),
+Bbb(nn: "Archive this course", a: Icon(Icons.save_alt_sharp)),
+Bbb(nn: "Report a playback problems", a: Icon(Icons.pest_control)),
+];
