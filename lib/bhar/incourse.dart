09aebc40e4a32c322c5d4f6994e694218a56a1d8
buhar <bhar.mayi01@gmail.com>
2023-03-19 16:00:10 +0300

    co+img+efc

--- lib/bhar/incourse.dart
+++ lib/bhar/incourse.dart
@@ -2,13 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class Aaa extends StatelessWidget {
-  const Aaa({super.key});
+class InCourse extends StatefulWidget {
+  const InCourse({super.key});
 
   @override
+  State<InCourse> createState() => _InCourseState();
+}
+
+class _InCourseState extends State<InCourse> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return  Scaffold(
+      body:Column(
+          children: [
+            Container(height: 250,decoration: BoxDecoration(image: DecorationImage
+                (image: AssetImage("images/php-course.jpg"),colorFilter: ColorFilter.mode(Color.fromARGB(115, 0, 0, 0),BlendMode.hardLight),
+                fit: BoxFit.cover)),)
+
+          ],
+
+
+      )
 
     );
   }
