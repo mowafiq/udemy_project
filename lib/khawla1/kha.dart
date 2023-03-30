9e1cc7278ebdbb3c6b770b665d9049264ae0376f
Francisco Rodriguez <weareidt@gmail.com>
2023-03-31 00:00:31 +0300

    pass

---
+++ lib/khawla1/kha.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Mul extends StatelessWidget {
+  const Mul({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+      
+    );
+  }
+}
