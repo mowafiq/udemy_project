8d0fa27d25d119f9f1bd486edac952011ed72fb2
Francisco Rodriguez <weareidt@gmail.com>
2023-03-30 20:54:17 +0300

    pass

--- lib/khawla1/h.dart
+++ lib/khawla1/h.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
@@ -6,6 +7,69 @@ class Mkk extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text("udmy")),
+      body: ListView(children: [
+        Positioned(
+          top: 0,
+          child: Container(
+            child: Image(image: AssetImage('imag/wo.PNG')),
+          ),
+        ),
+        Positioned(
+          child: Text(
+            "Learning that fitS ",
+            style: TextStyle(
+                color: Color.fromARGB(255, 26, 25, 24),
+                fontSize: 30,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        Positioned(
+          child: Text(
+            " Skills for your present(and future) ",
+            style: TextStyle(
+              color: Color.fromARGB(255, 26, 25, 24),
+              fontSize: 15,
+            ),
+          ),
+        ),
+        Container(
+          width: 200,
+          height: 70,
+          color: Color.fromARGB(148, 104, 160, 40),
+          child: Center(child: Text("FUTURE - READY SKILLY ON YOUR SCHEDULEs")),
+        ),
+        ListTile(
+          title: Text(
+            "Categories",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Wrap(
+          children: [
+            FilterChip(label: Text("Development"), onSelected: (bool b) {}),
+            FilterChip(label: Text("Business"), onSelected: (bool b) {}),
+            FilterChip(
+                label: Text("Personal Development"), onSelected: (bool b) {}),
+            FilterChip(label: Text("it& software"), onSelected: (bool b) {}),
+            FilterChip(
+                label: Text("Finance &Accounting "), onSelected: (bool b) {}),
+            FilterChip(label: Text(""), onSelected: (bool b) {}),
+            FilterChip(label: Text("text1"), onSelected: (bool b) {}),
+            FilterChip(label: Text("text1"), onSelected: (bool b) {}),
+            FilterChip(label: Text("text1"), onSelected: (bool b) {}),
+            FilterChip(label: Text("text1"), onSelected: (bool b) {}),
+            FilterChip(label: Text("text1"), onSelected: (bool b) {}),
+            FilterChip(label: Text("text1"), onSelected: (bool b) {}),
+          ],
+        ),
+        Positioned(
+            child: Text(
+          "Top courses in Design",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ))
+      ]),
+    );
   }
 }
--- lib/khawla1/kh1.dart
+++ lib/khawla1/kh1.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'h.dart';
+
 void main() {
   runApp(const MyApp());
 }
