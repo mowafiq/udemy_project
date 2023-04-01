c399a22d880ca8831da4e85cbd521eb8e1305d91
Hanaanajih <hanaanajih@gmail.com>
2023-04-02 01:49:54 +0300

    creating 4 fake data

--- lib/hanaa.dart/data.dart
+++ lib/hanaa.dart/data.dart
@@ -4,3 +4,24 @@ class CourseData {
 
   CourseData({required this.courseDesc, required this.authorName});
 }
+
+List<CourseData> CourseFakeData = [
+  CourseData(
+    authorName: "Hanaa Najih",
+    courseDesc: "Adobe Illistrator crash course for beignner, complete course",
+  ),
+  CourseData(
+    authorName: "Viyan Hashim",
+    courseDesc: "HTML & CSS Course for Advanced student, limited offer",
+  ),
+  CourseData(
+    authorName: "Khawla Sabah",
+    courseDesc:
+        "Unity Course for begineer, complete course with source code for every tutorial",
+  ),
+  CourseData(
+    authorName: "Buhar Erfan",
+    courseDesc:
+        "Forex tutorial for truly beginner, with giving good target for your first traiding",
+  )
+];
