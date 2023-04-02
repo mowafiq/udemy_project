969249d52af8543e1244161055d54dbeeaa7cca6
Hanaanajih <hanaanajih@gmail.com>
2023-04-03 01:58:21 +0300

    i finished from my screen

--- lib/hanaa.dart/data.dart
+++ lib/hanaa.dart/data.dart
@@ -1,27 +1,37 @@
+import 'package:flutter/material.dart';
+
 class CourseData {
+  final image;
   String courseDesc;
   String authorName;
 
-  CourseData({required this.courseDesc, required this.authorName});
+  CourseData(
+      {required this.image,
+      required this.courseDesc,
+      required this.authorName});
 }
 
 List<CourseData> CourseFakeData = [
   CourseData(
+    image: AssetImage('images/ai.png'),
     authorName: "Hanaa Najih",
-    courseDesc: "Adobe Illistrator crash course for beignner, complete course",
+    courseDesc: 'Adobe Illistrator crash course for beignner, complete course',
   ),
   CourseData(
+    image: AssetImage('images/html&css.jpg'),
     authorName: "Viyan Hashim",
-    courseDesc: "HTML & CSS Course for Advanced student, limited offer",
+    courseDesc: "HTML & CSS Course for Advanced student,limited offer join ",
   ),
   CourseData(
+    image: AssetImage('images/unity.jpg'),
     authorName: "Khawla Sabah",
     courseDesc:
-        "Unity Course for begineer, complete course with source code for every tutorial",
+        "Unity Course for begineer, with source code for every tutorial",
   ),
   CourseData(
+    image: AssetImage('images/forex.jpg'),
     authorName: "Buhar Erfan",
     courseDesc:
-        "Forex tutorial for truly beginner, with giving good target for your first traiding",
+        "Forex tutorial for beginner,with giving good target, come join",
   )
 ];
