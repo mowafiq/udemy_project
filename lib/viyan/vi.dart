1e0234e41a94339f45b1233b2078d738f82bcf4d
Francisco Rodriguez <weareidt@gmail.com>
2023-04-02 18:38:34 -0700

    pass

--- lib/viyan/vi.dart
+++ lib/viyan/vi.dart
@@ -2,4 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../controllers/search_controller.dart';
 
-class MyApp
+class SearchPage extends GetView<SearchController>{
+  const SearchPage({super.key});
+  @ovverride
+
+  Future<Widget> build (BuildContext context) async {
+    return Scaffold(
+      appBar: AppBar(
+        shape:const Border(bottom: BorderSide()), 
+        title: const TextField(
+          decoration: InputDecoration.collapsed(
+            hintText: "search for a resource",
+          ),
+          autofocus: true,
+        ),
+      ),
+      body: const Center(
+        child: Text("Search Page"),
+      ),
+    );
+  }
+}
