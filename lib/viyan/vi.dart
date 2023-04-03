fc4489fc7e383ad546251afb48fea658de888a0a
Francisco Rodriguez <weareidt@gmail.com>
2023-04-03 14:40:24 -0700

    pass

--- lib/viyan/vi.dart
+++ lib/viyan/vi.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import '../../../controllers/search_controller.dart';
+import 'package:searchfield/searchfield.dart';
 
-class SearchPage extends GetView<SearchController>{
-  const SearchPage({super.key});
-  @ovverride
 
-  Future<Widget> build (BuildContext context) async {
+
+
+class SearchPage extends GetView<SearchField>{
+const SearchPage ({super.key});
+  @override
+   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        shape:const Border(bottom: BorderSide()), 
+        shape:const Border(bottom: BorderSide(
+          color:Get.theme.secondaryHeaderColor.withOpacity(0.3)
+        )),
         title: const TextField(
-          decoration: InputDecoration.collapsed(
-            hintText: "search for a resource",
-          ),
+          decoration: InputDecoration.collapsed(hintText: "search for a resource"),
           autofocus: true,
         ),
       ),
       body: const Center(
-        child: Text("Search Page"),
+        child: Text("_"),
       ),
     );
-  }
-}
+  }}
